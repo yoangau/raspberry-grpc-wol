bfcff3f09d78533693095b4351ae5f079aecd7f2
Yoan Gauthier <yoangau@gmail.com>
2019-10-25 10:53:37 -0400

    new argument

--- client/args_parser.py
+++ client/args_parser.py
@@ -9,3 +9,5 @@ class ArgsParser:
     def __init__(self):
         self.parser = argparse.ArgumentParser(description='Process some integers.')
         self.parser.add_argument('option', type=str, choices=[self.power_on, self.power_off, self.hard_reset])
+        self.parser.add_argument("-i", help="path to id_rsa file", required=True, dest='key_file', type=str,
+                                 action="store_true")
--- client/desk_wol_client.py
+++ client/desk_wol_client.py
@@ -11,20 +11,20 @@ class PowerClient:
         self.args_parser = ArgsParser()
         self.channel = grpc.insecure_channel('localhost:50051')
         self.stub = desk_wol_pb2_grpc.PowerStub(self.channel)
-        self.signature = SignatureEncrypter.encrypt_signature("tests/id_rsa_test")
 
     def exec(self):
-        option = self.args_parser.parser.parse_args().option
-        power_request = desk_wol_pb2.PowerRequest(token=self.signature)
+        args = self.args_parser.parser.parse_args()
+        signature = SignatureEncrypter.encrypt_signature(args.key_file)
+        power_request = desk_wol_pb2.PowerRequest(token=signature)
         status_response: desk_wol_pb2.StatusResponse = None
 
-        if option == ArgsParser.power_on:
+        if args.option == ArgsParser.power_on:
             status_response = self.stub.PowerOn(power_request)
 
-        if option == ArgsParser.power_off:
+        if args.option == ArgsParser.power_off:
             status_response = self.stub.PowerOff(power_request)
 
-        if option == ArgsParser.hard_reset:
+        if args.option == ArgsParser.hard_reset:
             status_response = self.stub.HardReset(power_request)
 
         if status_response:
