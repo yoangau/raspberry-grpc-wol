57f788f43ccab9a91689cf5f95f76fa07e8b2dd2
Yoan Gauthier <yoangau@gmail.com>
2019-10-24 21:11:51 -0400

    implement encrypter

---
+++ client/signature_encrypter.py
@@ -0,0 +1,14 @@
+from Crypto.Signature import pss
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
+from common.signature import signature_message
+
+
+class SignatureEncrypter:
+
+    @staticmethod
+    def encrypt_signature(key_file: str) -> str:
+        key = RSA.import_key(open(key_file).read())
+        h = SHA256.new(signature_message)
+        signature = pss.new(key).sign(h)
+        return signature
