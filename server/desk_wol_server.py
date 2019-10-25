0b8fc8a97b7a8164115d732f4c074d3e76a636b8
Yoan Gauthier <yoangau@gmail.com>
2019-10-25 12:08:54 -0400

    rename server service

--- server/desk_wol_server.py
+++ server/desk_wol_server.py
@@ -7,7 +7,7 @@ import protos.pythonpb2.desk_wol_pb2_grpc as dw_pb2_grpc
 from server.signature_decrypter import SignatureDecrypter
 
 
-class PowerServicer(dw_pb2_grpc.PowerServicer):
+class PowerService(dw_pb2_grpc.PowerServicer):
 
     @staticmethod
     def check_signature(request: dw_pb2.PowerRequest) -> dw_pb2.StatusResponse:
@@ -16,7 +16,7 @@ class PowerServicer(dw_pb2_grpc.PowerServicer):
                 "/home/yoangau/Documents/raspberry-grpc-wol/tests/id_rsa_test.pub", request.token))
 
     def PowerOn(self, request, context):
-        status = PowerServicer.check_signature(request)
+        status = PowerService.check_signature(request)
 
         if status.info is SignatureDecrypter.invalid_signature:
             return status
@@ -25,7 +25,7 @@ class PowerServicer(dw_pb2_grpc.PowerServicer):
         return dw_pb2.StatusResponse(info=f"Power on ...")
 
     def PowerOff(self, request, context):
-        status = PowerServicer.check_signature(request)
+        status = PowerService.check_signature(request)
 
         if status.info is SignatureDecrypter.invalid_signature:
             return status
@@ -34,7 +34,7 @@ class PowerServicer(dw_pb2_grpc.PowerServicer):
         return dw_pb2.StatusResponse(info=f"Power off ...")
 
     def HardReset(self, request, context):
-        status = PowerServicer.check_signature(request)
+        status = PowerService.check_signature(request)
 
         if status.info is SignatureDecrypter.invalid_signature:
             return status
@@ -46,7 +46,7 @@ class PowerServicer(dw_pb2_grpc.PowerServicer):
     def serve():
         server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
         dw_pb2_grpc.add_PowerServicer_to_server(
-            PowerServicer(), server)
+            PowerService(), server)
         server.add_insecure_port('[::]:50051')
         server.start()
         server.wait_for_termination()
