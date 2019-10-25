819a26a5764ffc178ba225a2479b655a8f177e2b
Yoan Gauthier <yoangau@gmail.com>
2019-10-25 00:21:14 -0400

    change sh to fish

--- setup.fish
+++ setup.fish
@@ -2,4 +2,4 @@
 
 pip install -r requirements.txt
 python -m grpc_tools.protoc -Iprotos --python_out=protos/out --grpc_python_out=protos/out protos/desk_wol.proto
-set -x PYTHONPATH (pwd)/protos/out:(pwd)/protos:(pwd)
+set -x PYTHONPATH (pwd)/protos/out:(pwd)/protos:(pwd):(pwd)/client
