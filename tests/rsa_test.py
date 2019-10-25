42775bb9277b084bd4ce75f31b294a0494ced977
Yoan Gauthier <yoangau@gmail.com>
2019-10-25 09:21:34 -0400

    add test bad signature

--- tests/rsa_test.py
+++ tests/rsa_test.py
@@ -4,10 +4,14 @@ from server.signature_decrypter import SignatureDecrypter
 
 
 class RSATest(unittest.TestCase):
-    def test_signature(self):
+    def test_signature_should_work(self):
         signature = SignatureEncrypter.encrypt_signature("id_rsa_test")
         self.assertEqual(SignatureDecrypter.decrypt_signature("id_rsa_test.pub", signature),
                          SignatureDecrypter.valid_signature)
+
+    def test_signature_should_not_work(self):
+        self.assertEqual(SignatureDecrypter.decrypt_signature("id_rsa_test.pub", bytes("not_valid", encoding='utf-8')),
+                         SignatureDecrypter.invalid_signature)
 
 
 if __name__ == '__main__':
