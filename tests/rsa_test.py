85f30f401330c5a0a7661027b0c2381d72688eaf
Yoan Gauthier <yoangau@gmail.com>
2019-10-24 21:35:50 -0400

    start test for signature

--- tests/rsa_test.py
+++ tests/rsa_test.py
@@ -1,9 +1,13 @@
 import unittest
+from client.signature_encrypter import SignatureEncrypter
+from server.signature_decrypter import SignatureDecrypter
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class RSATest(unittest.TestCase):
+    def test_signature(self):
+        signature = SignatureEncrypter.encrypt_signature("id_rsa_test")
+        self.assertEqual(SignatureDecrypter.decrypt_signature("id_rsa_test.pub", signature),
+                         SignatureDecrypter.valid_signature)
 
 
 if __name__ == '__main__':
