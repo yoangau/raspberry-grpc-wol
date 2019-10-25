5609030a14daf46b19053dc57470043a483ffb21
Yoan Gauthier <yoangau@gmail.com>
2019-10-24 21:34:54 -0400

    normalise decrypt impementation

--- server/signature_decrypter.py
+++ server/signature_decrypter.py
@@ -5,14 +5,16 @@ from common.signature import signature_message
 
 
 class SignatureDecrypter:
+    valid_signature: str = "The signature is valid."
+    invalid_signature: str = "The signature is not valid."
 
     @staticmethod
-    def decrypt(signature: bytes) -> str:
-        key = RSA.import_key(open('public_key.der').read())
+    def decrypt_signature(key_file: str, signature: bytes) -> str:
+        key = RSA.import_key(open(key_file).read())
         h = SHA256.new(signature_message)
         verifier = pss.new(key)
         try:
             verifier.verify(h, signature)
-            return "The signature is valid."
+            return SignatureDecrypter.valid_signature
         except (ValueError, TypeError):
-            return "The signature is not valid."
+            return SignatureDecrypter.invalid_signature
